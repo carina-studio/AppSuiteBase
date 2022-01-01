5d9fb647986fc0cc5080f768bc758d1489723c7f
Hamster <hamster620@gmail.com>
2022-01-01 10:46:40 +0800

    Update style of tab strip of TabControl.

---
+++ Core/Controls/TabStripScrollViewer.cs
@@ -0,0 +1,90 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using CarinaStudio.Animation;
+using CarinaStudio.Threading;
+using System;
+
+namespace CarinaStudio.AppSuite.Controls
+{
+    internal class TabStripScrollViewer : ScrollViewer
+    {
+        // Fields.
+        readonly ScheduledAction correctOffsetAction;
+        VectorAnimator? offsetAnimator;
+
+
+        // Constructor.
+        public TabStripScrollViewer()
+        {
+            this.correctOffsetAction = new ScheduledAction(() =>
+            {
+                var extent = this.Extent;
+                var viewport = this.Viewport;
+                var offset = this.Offset;
+                if (offset.X + viewport.Width > extent.Width && offset.X > 0)
+                {
+                    if (this.offsetAnimator != null)
+                    {
+                        this.offsetAnimator.Cancel();
+                        this.offsetAnimator = null;
+                    }
+                    this.Offset = new Vector(extent.Width - viewport.Width, 0);
+                }
+            });
+        }
+
+
+        // Property changed.
+        protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
+        {
+            base.OnPropertyChanged(change);
+            if (change.Property == ExtentProperty || change.Property == ViewportProperty)
+                this.correctOffsetAction.Schedule();
+        }
+
+
+        // Scroll by given offset.
+        void ScrollBy(double offset)
+        {
+            // get current offset
+            var currentOffset = this.offsetAnimator?.EndValue ?? this.Offset;
+
+            // cancel current animation
+            if (this.offsetAnimator != null)
+                this.offsetAnimator.Cancel();
+
+            // calculate target offset
+            var targetOffsetX = currentOffset.X + offset;
+            var extent = this.Extent;
+            var viewport = this.Viewport;
+            if (targetOffsetX < 0)
+                targetOffsetX = 0;
+            else if (targetOffsetX + viewport.Width > extent.Width)
+                targetOffsetX = extent.Width - viewport.Width;
+
+            // animate offset
+            var duration = AppSuiteApplication.CurrentOrNull?.FindResource("TimeSpan/Animation.Fast").Let(it =>
+            {
+                if (it is TimeSpan timeSpan)
+                    return timeSpan;
+                return TimeSpan.FromMilliseconds(250);
+            }) ?? TimeSpan.FromMilliseconds(250);
+            this.offsetAnimator = new VectorAnimator(currentOffset, new Vector(targetOffsetX, currentOffset.Y)).Also(it =>
+            {
+                it.Completed += (_, e) => this.offsetAnimator = null;
+                it.Duration = duration;
+                it.Interpolator = Interpolators.Deleceleration;
+                it.ProgressChanged += (_, e) => this.Offset = it.Value;
+                it.Start();
+            });
+        }
+
+
+        // Scroll left.
+        void ScrollLeft() => ScrollBy(-100);
+
+
+        // Scroll right.
+        void ScrollRight() => ScrollBy(100);
+    }
+}
---
+++ Core/Converters/TabStripScrollingButtonVisibilityConverter.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace CarinaStudio.AppSuite.Converters
+{
+    internal class TabStripScrollingButtonVisibilityConverter : IMultiValueConverter
+    {
+        // Static fields.
+        public static readonly TabStripScrollingButtonVisibilityConverter Default = new TabStripScrollingButtonVisibilityConverter();
+
+
+        /// <inheritdoc/>
+        public object? Convert(IList<object> values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (typeof(object) != targetType && typeof(bool) != targetType)
+                return null;
+            if (values.Count >= 3
+                && values[0] is double offset
+                && values[1] is double extentWidth
+                && values[2] is double viewportWidth)
+            {
+                if ((parameter as string) == "Right")
+                    return (extentWidth > viewportWidth && (offset + viewportWidth) < extentWidth - 1);
+                return (extentWidth > viewportWidth && offset > 1);
+            }
+            return false;
+        }
+
+
+        /// <inheritdoc/>
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
+    }
+}
