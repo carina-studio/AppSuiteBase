ff898cbf4c807de67cc993485d158c10031558f2
Hamster <hamster620@gmail.com>
2022-01-20 18:51:50 +0800

    Add "Select all" and "Clear" items in context menu of TextBox.

--- Core/Converters/ValueToBooleanConverters.cs
+++ Core/Converters/ValueToBooleanConverters.cs
@@ -10,6 +10,14 @@ namespace CarinaStudio.AppSuite.Converters
     public static class ValueToBooleanConverters
     {
         /// <summary>
+        /// Convert to True if value is empty string.
+        /// </summary>
+        public static readonly IValueConverter EmptyStringToTrue = new EmptyStringToBooleanConverterImpl(true);
+        /// <summary>
+        /// Convert to True if value is non-empty string.
+        /// </summary>
+        public static readonly IValueConverter NonEmptyStringToTrue = new EmptyStringToBooleanConverterImpl(false);
+        /// <summary>
         /// Convert to True if value is non-Null.
         /// </summary>
         public static readonly IValueConverter NonNullToTrue = new NullToBooleanConverterImpl(false);
@@ -17,6 +25,32 @@ namespace CarinaStudio.AppSuite.Converters
         /// Convert to True if value is Null.
         /// </summary>
         public static readonly IValueConverter NullToTrue = new NullToBooleanConverterImpl(true);
+
+
+        // Convert from empty/non-empty string to boolean.
+        class EmptyStringToBooleanConverterImpl : IValueConverter
+        {
+            // Fields.
+            readonly bool emptyIsTrue;
+
+            // Constructor.
+            public EmptyStringToBooleanConverterImpl(bool emptyIsTrue) => this.emptyIsTrue = emptyIsTrue;
+
+            // Convert.
+            public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+            {
+                if (typeof(object) != targetType && typeof(bool) != targetType)
+                    return null;
+                if (value == null)
+                    return this.emptyIsTrue;
+                if (value is string str)
+                    return this.emptyIsTrue ? str.Length == 0 : str.Length > 0;
+                return null;
+            }
+
+            // Convert back.
+            public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => null;
+        }
 
 
         // Convert from null/non-null to boolean.
