7fa9d08e52070d46252a4a7f203909e640555dfe
Hamster <hamster620@gmail.com>
2022-04-08 09:51:27 +0800

    Use overlay to fading content of window instead of changing opacity of content.

--- Core/Controls/WindowContentFadingHelper.cs
+++ Core/Controls/WindowContentFadingHelper.cs
@@ -25,6 +25,7 @@ namespace CarinaStudio.AppSuite.Controls
         // Fields.
         Control? content;
         DoubleAnimator? contentFadingAnimator;
+        Control? contentFadingOverlay;
         readonly ScheduledAction fadeInContentAction;
 
 
@@ -34,21 +35,36 @@ namespace CarinaStudio.AppSuite.Controls
             // create scheduled action
             this.fadeInContentAction = new ScheduledAction(() =>
             {
-                if (this.content == null)
+                if (this.content == null && this.contentFadingOverlay == null)
                     return;
                 this.contentFadingAnimator?.Cancel();
-                this.contentFadingAnimator = new DoubleAnimator(this.content.Opacity, 1.0).Also(it =>
+                this.contentFadingAnimator = this.contentFadingOverlay != null
+                    ? new DoubleAnimator(this.contentFadingOverlay.Opacity, 0)
+                    : new DoubleAnimator(this.content.AsNonNull().Opacity, 1.0);
+                this.contentFadingAnimator.Let(it =>
                 {
                     it.Completed += (_, e) =>
                     {
-                        this.content.Opacity = it.EndValue;
+                        if (this.contentFadingOverlay != null)
+                        {
+                            this.contentFadingOverlay.IsVisible = false;
+                            this.contentFadingOverlay.Opacity = it.EndValue;
+                        }
+                        else if (this.content != null)
+                            this.content.Opacity = it.EndValue;
                         this.contentFadingAnimator = null;
                         this.IsFadingContent = false;
                         this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsFadingContent)));
                     };
                     it.Duration = ContentFadeInDuration;
                     it.Interpolator = Interpolators.Deceleration;
-                    it.ProgressChanged += (_, e) => this.content.Opacity = it.Value;
+                    it.ProgressChanged += (_, e) =>
+                    {
+                        if (this.contentFadingOverlay != null)
+                            this.contentFadingOverlay.Opacity = it.Value;
+                        else if (this.content != null)
+                            this.content.Opacity = it.Value;
+                    };
                     it.Start();
                 });
                 if (!this.IsFadingContent)
@@ -74,30 +90,44 @@ namespace CarinaStudio.AppSuite.Controls
                     this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsFadingContent)));
                 }
                 this.content = content as Control;
-                if (this.content != null)
+                if (this.content != null && this.contentFadingOverlay == null)
                     this.content.Opacity = window.HasDialogs ? ContentFadeOutOpacity : 1.0;
             });
             window.GetObservable(CarinaStudio.Controls.Window.HasDialogsProperty).Subscribe(hasDialogs =>
             {
-                if (this.content == null)
+                if (this.content == null && this.contentFadingOverlay == null)
                     return;
                 if (hasDialogs)
                 {
                     this.fadeInContentAction.Cancel();
                     this.contentFadingAnimator?.Cancel();
-                    this.contentFadingAnimator = new DoubleAnimator(this.content.Opacity, ContentFadeOutOpacity).Also(it =>
+                    this.contentFadingAnimator = this.contentFadingOverlay != null
+                        ? new DoubleAnimator(this.contentFadingOverlay.Opacity, 1 - ContentFadeOutOpacity)
+                        : new DoubleAnimator(this.content.AsNonNull().Opacity, ContentFadeOutOpacity);
+                    this.contentFadingAnimator.Let(it =>
                     {
                         it.Completed += (_, e) =>
                         {
-                            this.content.Opacity = it.EndValue;
+                            if (this.contentFadingOverlay != null)
+                                this.contentFadingOverlay.Opacity = it.EndValue;
+                            else if (this.content != null)
+                                this.content.Opacity = it.EndValue;
                             this.contentFadingAnimator = null;
                             this.IsFadingContent = false;
                             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsFadingContent)));
                         };
                         it.Duration = ContentFadeOutDuration;
                         it.Interpolator = Interpolators.Deceleration;
-                        it.ProgressChanged += (_, e) => this.content.Opacity = it.Value;
+                        it.ProgressChanged += (_, e) =>
+                        {
+                            if (this.contentFadingOverlay != null)
+                                this.contentFadingOverlay.Opacity = it.Value;
+                            else if (this.content != null)
+                                this.content.Opacity = it.Value;
+                        };
                         it.Start();
+                        if (this.contentFadingOverlay != null)
+                            this.contentFadingOverlay.IsVisible = true;
                     });
                     if (!this.IsFadingContent)
                     {
@@ -115,6 +145,27 @@ namespace CarinaStudio.AppSuite.Controls
                     this.fadeInContentAction.Schedule(ContentFadeInDelay);
                 }
             });
+            window.TemplateApplied += (_, e) =>
+            {
+                this.fadeInContentAction.Cancel();
+                if (this.contentFadingAnimator != null)
+                {
+                    this.contentFadingAnimator.Cancel();
+                    this.contentFadingAnimator = null;
+                }
+                if (this.IsFadingContent)
+                {
+                    this.IsFadingContent = false;
+                    this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsFadingContent)));
+                }
+                this.contentFadingOverlay = e.NameScope.Find<Control>("PART_ContentFadingOverlay");
+                if (this.contentFadingOverlay != null)
+                {
+                    if (this.content != null)
+                        this.content.Opacity = 1;
+                    this.contentFadingOverlay.Opacity = window.HasDialogs ? 1 - ContentFadeOutOpacity : 0;
+                }
+            };
         }
 
 
