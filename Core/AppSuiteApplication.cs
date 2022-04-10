83c3521ec443f5bebf712ce25929a695a8c12aac
Hamster <hamster620@gmail.com>
2022-04-10 13:28:27 +0800

    Add header background in ApplicationInfoDialog.

--- Core/AppSuiteApplication.cs
+++ Core/AppSuiteApplication.cs
@@ -2780,6 +2780,10 @@ namespace CarinaStudio.AppSuite
                 this.accentColorResources["SystemAccentColorLight1"] = sysAccentColorLight1;
                 this.accentColorResources["SystemAccentColorLight2"] = GammaTransform(accentColor, 0.431);
                 this.accentColorResources["SystemAccentColorLight3"] = GammaTransform(accentColor, 0.006);
+                this.accentColorResources["Color/Accent.WithOpacity.0.75"] = Color.FromArgb((byte)(accentColor.A * 0.75 + 0.5), accentColor.R, accentColor.G, accentColor.B);
+                this.accentColorResources["Color/Accent.WithOpacity.0.5"] = Color.FromArgb((byte)(accentColor.A * 0.5 + 0.5), accentColor.R, accentColor.G, accentColor.B);
+                this.accentColorResources["Color/Accent.WithOpacity.0.25"] = Color.FromArgb((byte)(accentColor.A * 0.25 + 0.5), accentColor.R, accentColor.G, accentColor.B);
+                this.accentColorResources["Color/Accent.Transparent"] = Color.FromArgb(0, accentColor.R, accentColor.G, accentColor.B);
 
                 // icon colors
                 this.accentColorResources["Brush.Icon.Active"] = new SolidColorBrush(sysAccentColorLight1);
