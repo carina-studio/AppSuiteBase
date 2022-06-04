a51ac545eae2b9bc1a14eaf7ec8a46d4e1051a49
Hamster <hamster620@gmail.com>
2022-06-04 19:40:18 +0800

    Add build script for macOS and Linux.

---
+++ BuildPackages.sh
@@ -0,0 +1,22 @@
+PROJECT_LIST=("Core" "Core.Tests")
+
+# Reset output directory
+rm -r ./Packages
+mkdir ./Packages
+if [ "$?" != "0" ]; then
+    exit
+fi
+
+# Build packages
+for i in "${!PROJECT_LIST[@]}"; do
+    PROJECT=${PROJECT_LIST[$i]}
+
+    # Build
+    dotnet build $PROJECT -c Release
+    if [ "$?" != "0" ]; then
+        exit
+    fi
+
+    # Package
+    dotnet pack $PROJECT -c Release -o ./Packages
+done
