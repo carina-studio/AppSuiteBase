e76a85b9cd83f6eb0e0aa0c19b921d7e31e00183
Hamster <hamster620@gmail.com>
2023-03-14 20:40:14 +0800

    Define more functions in ScriptGlobals<T>.

--- Core/Scripting/ScriptGlobals.cs
+++ Core/Scripting/ScriptGlobals.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using System.Threading;
 
 namespace CarinaStudio.AppSuite.Scripting;
@@ -32,4 +33,45 @@ public class ScriptGlobals<TContext> where TContext : IContext
 
     /// <inheritdoc/>
     public TContext Context { get; }   
+
+
+    /// <summary>
+    /// Check whether cancellation of running scriot has been requested or not.
+    /// </summary>
+    public bool IsCancellationRequested => this.CancellationToken.IsCancellationRequested;
+
+
+    /// <summary>
+    /// Write log with <see cref="LogLevel.Error"/> level.
+    /// </summary>
+    /// <param name="obj">Message of log.</param>
+    public void LogError(object? obj) => this.Context.Logger.LogError("{msg}", obj);
+
+
+    /// <summary>
+    /// Write log with <see cref="LogLevel.Debug"/> level.
+    /// </summary>
+    /// <param name="obj">Message of log.</param>
+    public void LogDebug(object? obj) => this.Context.Logger.LogDebug("{msg}", obj);
+
+
+    /// <summary>
+    /// Write log with <see cref="LogLevel.Information"/> level.
+    /// </summary>
+    /// <param name="obj">Message of log.</param>
+    public void LogInfo(object? obj) => this.Context.Logger.LogInformation("{msg}", obj);
+
+
+    /// <summary>
+    /// Write log with <see cref="LogLevel.Trace"/> level.
+    /// </summary>
+    /// <param name="obj">Message of log.</param>
+    public void LogTrace(object? obj) => this.Context.Logger.LogTrace("{msg}", obj);
+
+
+    /// <summary>
+    /// Write log with <see cref="LogLevel.Warning"/> level.
+    /// </summary>
+    /// <param name="obj">Message of log.</param>
+    public void LogWarning(object? obj) => this.Context.Logger.LogWarning("{msg}", obj);
 }
