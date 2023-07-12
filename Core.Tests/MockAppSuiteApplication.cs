e9f13fca0974a90e4e64fd694e60949f2f833243
Hamster <hamster620@gmail.com>
2023-07-12 15:33:54 +0800

    Upgrade to AppBase 2.1.5.712-preview.

--- Core.Tests/MockAppSuiteApplication.cs
+++ Core.Tests/MockAppSuiteApplication.cs
@@ -437,11 +437,7 @@ namespace CarinaStudio.AppSuite
 
 
         /// <inheritdoc/>
-        SynchronizationContext ISynchronizable.SynchronizationContext => synchronizationContext ?? throw new InvalidOperationException("Application instance is not ready.");
-
-
-        /// <inheritdoc/>
-        public virtual DispatcherSynchronizationContext SynchronizationContext => throw new NotImplementedException();
+        public virtual SynchronizationContext SynchronizationContext => synchronizationContext ?? throw new InvalidOperationException("Application instance is not ready.");
         
         
         /// <inheritdoc/>
