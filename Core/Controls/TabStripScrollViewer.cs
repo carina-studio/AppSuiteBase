d7b90ab2eace4765ed3ac8894a8a065362194483
Hamster <hamster620@gmail.com>
2023-08-06 09:38:02 +0800

    Update style of TabStripScrollViewer.

--- Core/Controls/TabStripScrollViewer.cs
+++ Core/Controls/TabStripScrollViewer.cs
@@ -5,6 +5,9 @@ using Avalonia.Controls.Primitives;
 using CarinaStudio.Animation;
 using CarinaStudio.Threading;
 using System;
+using Avalonia.LogicalTree;
+using Avalonia.Media;
+using CarinaStudio.Controls;
 
 namespace CarinaStudio.AppSuite.Controls
 {
@@ -12,14 +15,24 @@ namespace CarinaStudio.AppSuite.Controls
     {
         // Fields.
         ScrollContentPresenter? contentPresenter;
-        readonly ScheduledAction correctOffsetAction;
+        private double maxEdgeFadingSize;
         VectorAnimator? offsetAnimator;
+        private readonly LinearGradientBrush opacityMackBrush = new LinearGradientBrush().Also(it =>
+        {
+            it.EndPoint = new(1.0, 0.0, RelativeUnit.Relative);
+            it.GradientStops.Add(new(Colors.Transparent, 0.0));
+            it.GradientStops.Add(new(Colors.Black, 0.0));
+            it.GradientStops.Add(new(Colors.Black, 1.0));
+            it.GradientStops.Add(new(Colors.Transparent, 1.0));
+            it.StartPoint = new(0.0, 0.0, RelativeUnit.Relative);
+        });
 
 
         // Constructor.
         public TabStripScrollViewer()
         {
-            this.correctOffsetAction = new ScheduledAction(() =>
+            // prepare actions
+            var correctOffsetAction = new ScheduledAction(() =>
             {
                 var extent = this.Extent;
                 var viewport = this.Viewport;
@@ -34,18 +47,64 @@ namespace CarinaStudio.AppSuite.Controls
                     this.Offset = new Vector(extent.Width - viewport.Width, 0);
                 }
             });
-            this.GetObservable(ExtentProperty).Subscribe(_ => this.correctOffsetAction.Schedule());
+            var updateOpacityMaskAction = new ScheduledAction(() =>
+            {
+                var size = this.Bounds.Size;
+                if (size.Width <= 0 || size.Height <= 0)
+                    return;
+                var brush = this.opacityMackBrush;
+                if (this.CanHorizontallyScroll)
+                {
+                    if (this.CanVerticallyScroll)
+                    {
+                        brush.GradientStops[1].Offset = 0.0;
+                        brush.GradientStops[2].Offset = 1.0;
+                    }
+                    else
+                    {
+                        var offset = this.Offset;
+                        brush.EndPoint = new(1.0, 0.0, RelativeUnit.Relative);
+                        brush.GradientStops[1].Offset = Math.Min(0.4, Math.Min(offset.X, this.maxEdgeFadingSize) / size.Width);
+                        brush.GradientStops[2].Offset = 1.0 - Math.Min(0.4, Math.Min(this.Extent.Width - (offset.X + this.Viewport.Width), this.maxEdgeFadingSize) / size.Width);
+                    }
+                }
+                else if (this.CanVerticallyScroll)
+                {
+                    var offset = this.Offset;
+                    brush.EndPoint = new(0.0, 1.0, RelativeUnit.Relative);
+                    brush.GradientStops[1].Offset = Math.Min(0.4, Math.Min(offset.Y, this.maxEdgeFadingSize) / size.Height);
+                    brush.GradientStops[2].Offset = 1.0 - Math.Min(0.4, Math.Min(this.Extent.Height - (offset.Y + this.Viewport.Height), this.maxEdgeFadingSize) / size.Height);
+                }
+                else
+                {
+                    brush.GradientStops[1].Offset = 0.0;
+                    brush.GradientStops[2].Offset = 1.0;
+                }
+                this.contentPresenter?.InvalidateVisual();
+            });
+            
+            // attach to self
+            this.GetObservable(ExtentProperty).Subscribe(_ =>
+            {
+                correctOffsetAction.Schedule();
+                updateOpacityMaskAction.Schedule();
+            });
             this.GetObservable(HorizontalScrollBarVisibilityProperty).Subscribe(visibility =>
             {
                 if (this.contentPresenter is not null)
                     this.contentPresenter.CanHorizontallyScroll = visibility == ScrollBarVisibility.Visible;
+                updateOpacityMaskAction.Schedule();
             });
+            this.GetObservable(OffsetProperty).Subscribe(_ => updateOpacityMaskAction.Schedule());
+            this.GetObservable(PaddingProperty).Subscribe(_ => updateOpacityMaskAction.Schedule());
+            this.SizeChanged += (_, _) => updateOpacityMaskAction.Schedule();
             this.GetObservable(VerticalScrollBarVisibilityProperty).Subscribe(visibility =>
             {
                 if (this.contentPresenter is not null)
                     this.contentPresenter.CanVerticallyScroll = visibility == ScrollBarVisibility.Visible;
+                updateOpacityMaskAction.Schedule();
             });
-            this.GetObservable(ViewportProperty).Subscribe(_ => this.correctOffsetAction.Schedule());
+            this.GetObservable(ViewportProperty).Subscribe(_ => correctOffsetAction.Schedule());
         }
         
         
@@ -56,8 +115,17 @@ namespace CarinaStudio.AppSuite.Controls
             this.contentPresenter = e.NameScope.Find<ScrollContentPresenter>("PART_ContentPresenter")?.Also(it =>
             {
                 it.CanHorizontallyScroll = this.HorizontalScrollBarVisibility == ScrollBarVisibility.Visible;
+                it.OpacityMask = this.opacityMackBrush;
                 it.CanVerticallyScroll = this.VerticalScrollBarVisibility == ScrollBarVisibility.Visible;
             });
+        }
+
+
+        /// <inheritdoc/>
+        protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e)
+        {
+            base.OnAttachedToLogicalTree(e);
+            this.maxEdgeFadingSize = this.FindResourceOrDefault("Double/TabControl.TabStrip.MaxEdgeFadingSize", 20.0);
         }
 
 
