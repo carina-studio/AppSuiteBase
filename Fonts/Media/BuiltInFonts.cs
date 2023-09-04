e6984de15b31c30542326539a518d4770d84b776
Hamster <hamster620@gmail.com>
2023-09-04 11:07:37 +0800

    [1] Export 'Inter' through BuiltInFonts. [2] Add BuildInFonts.OpenStream().

--- Fonts/Media/BuiltInFonts.cs
+++ Fonts/Media/BuiltInFonts.cs
@@ -2,6 +2,8 @@ using Avalonia.Media;
 using CarinaStudio.Collections;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using Avalonia.Platform;
 
 namespace CarinaStudio.AppSuite.Media;
 
@@ -11,9 +13,11 @@ namespace CarinaStudio.AppSuite.Media;
 public static class BuiltInFonts
 {
     // Fields.
+    static readonly Uri baseCoreResourceUri = new($"avares://CarinaStudio.AppSuite.Core/Fonts/");
     static readonly Uri baseResourceUri = new($"avares://{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}/Resources/Fonts/");
     static IList<FontFamily>? fontFamilies;
     static FontFamily? ibmPlexMono;
+    static FontFamily? inter;
     static FontFamily? notoSansSC;
     static FontFamily? notoSansTC;
     static FontFamily? roboto;
@@ -29,6 +33,7 @@ public static class BuiltInFonts
         fontFamilies ?? ListExtensions.AsReadOnly(new[]
         {
             IBMPlexMono,
+            Inter,
             NotoSansSC,
             NotoSansTC,
             Roboto,
@@ -44,15 +49,67 @@ public static class BuiltInFonts
     
     
     /// <summary>
+    /// Inter.
+    /// </summary>
+    public static FontFamily Inter => inter ?? new FontFamily(baseCoreResourceUri, "#Inter").Also(it => inter = it);
+    
+    
+    /// <summary>
     /// Noto Sans Simplified Chinese.
     /// </summary>
-    public static FontFamily NotoSansSC => notoSansSC ?? new FontFamily(new($"avares://CarinaStudio.AppSuite.Core/Fonts/"), "#Noto Sans SC").Also(it => notoSansSC = it);
+    public static FontFamily NotoSansSC => notoSansSC ?? new FontFamily(baseCoreResourceUri, "#Noto Sans SC").Also(it => notoSansSC = it);
     
     
     /// <summary>
     /// Noto Sans Traditional Chinese.
     /// </summary>
-    public static FontFamily NotoSansTC => notoSansTC ?? new FontFamily(new($"avares://CarinaStudio.AppSuite.Core/Fonts/"), "#Noto Sans TC").Also(it => notoSansTC = it);
+    public static FontFamily NotoSansTC => notoSansTC ?? new FontFamily(baseCoreResourceUri, "#Noto Sans TC").Also(it => notoSansTC = it);
+
+
+    /// <summary>
+    /// Open <see cref="Stream"/> of given built-in font.
+    /// </summary>
+    /// <param name="name">Name of built-in font.</param>
+    /// <param name="weight">Weight of font.</param>
+    /// <param name="style">Style of font.</param>
+    /// <returns><see cref="Stream"/> of given built-in font.</returns>
+    public static Stream OpenStream(string name, FontWeight weight = FontWeight.Regular, FontStyle style = FontStyle.Normal)
+    {
+        var uri = name switch
+        {
+            nameof(Inter) or nameof(NotoSansSC) or nameof(NotoSansTC) => Global.Run(() =>
+            {
+                weight = weight switch
+                {
+                    FontWeight.Bold => weight,
+                    _ => FontWeight.Regular,
+                };
+                if (weight == FontWeight.Regular)
+                    return new Uri(baseCoreResourceUri, $"{name}-Regular.ttf");
+                return new Uri(baseCoreResourceUri, $"{name}-{weight}.ttf");
+            }),
+            _ => Global.Run(() =>
+            {
+                weight = weight switch
+                {
+                    FontWeight.Bold or FontWeight.Light => weight,
+                    _ => FontWeight.Regular,
+                };
+                style = style switch
+                {
+                    FontStyle.Italic => style,
+                    _ => FontStyle.Normal,
+                };
+                string postfix;
+                if (weight == FontWeight.Regular)
+                    postfix = style == FontStyle.Normal ? "Regular" : $"{style}";
+                else
+                    postfix = style == FontStyle.Normal ? $"{weight}" : $"{weight}{style}";
+                return new Uri(baseResourceUri, $"{name}-{postfix}.ttf");
+            }),
+        };
+        return AssetLoader.Open(uri);
+    }
     
 
     /// <summary>
