6568022ccf4a45495ba73134bdf458036fae18ce
Hamster <hamster620@gmail.com>
2023-09-16 04:52:10 +0800

    Move Windows native symbols to Win32.

--- Core/AppSuiteApplication.Windows.cs
+++ Core/AppSuiteApplication.Windows.cs
@@ -10,69 +10,14 @@ using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
+using CarinaStudio.AppSuite.Native;
 
 namespace CarinaStudio.AppSuite;
 
 unsafe partial class AppSuiteApplication
 {
-    // Native COM interfaces.
-    [ComImport]
-    [Guid("ea1afb91-9e28-4b86-90e9-9e9f8a5eefaf")]
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    interface ITaskbarList3
-    {
-        // ITaskbarList
-        void HrInit();
-        void AddTab(IntPtr hwnd);
-        void DeleteTab(IntPtr hwnd);
-        void ActivateTab(IntPtr hwnd);
-        void SetActiveAlt(IntPtr hwnd);
-
-        // ITaskbarList2
-        void MarkFullscreenWindow(IntPtr hwnd, bool fFullscreen);
-
-        // ITaskbarList3
-        void SetProgressValue(IntPtr hwnd, ulong ullCompleted, ulong ullTotal);
-        void SetProgressState(IntPtr hwnd, uint tbpFlags);
-        void RegisterTab(IntPtr hwndTab, IntPtr hwndMDI);
-        void UnregisterTab(IntPtr hwndTab);
-        void SetTabOrder(IntPtr hwndTab, IntPtr hwndInsertBefore);
-        void SetTabActive(IntPtr hwndTab, IntPtr hwndMDI);
-        void ThumbBarAddButtons(IntPtr hwnd, uint cButtons, IntPtr pButton);
-        void ThumbBarUpdateButtons(IntPtr hwnd, uint cButtons, IntPtr pButton);
-        void ThumbBarSetImageList(IntPtr hwnd, IntPtr himl);
-        void SetOverlayIcon(IntPtr hwnd, IntPtr hIcon, string? pszDescription);
-        void SetThumbnailTooltip(IntPtr hwnd, string? pszTip);
-        void SetThumbnailClip(IntPtr hwnd, void* prcClip);
-    }
-
-
-    // Constants.
-    const uint CLSCTX_INPROC_SERVER = 0x1;
-    static readonly Guid CLSID_TaskBarList = new("56fdf344-fd6d-11d0-958a-006097c9a090");
-    const uint DWMWA_CAPTION_COLOR = 35;
-    const uint DWMWA_WINDOW_CORNER_PREFERENCE = 33;
-    const uint DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
-    const int DWMWCP_ROUND = 2;
-    static readonly Guid IID_TaskBarList3 = new("ea1afb91-9e28-4b86-90e9-9e9f8a5eefaf");
-    const uint TBPF_ERROR = 0x4;
-    const uint TBPF_INDETERMINATE = 0x1;
-    const uint TBPF_NOPROGRESS = 0x0;
-    const uint TBPF_NORMAL = 0x2;
-    const uint TBPF_PAUSED = 0x8;
-
-
-    // Native symbols.
-    [DllImport("Ole32", SetLastError = true)]
-    static extern int CoCreateInstance(in Guid rclsid, [MarshalAs(UnmanagedType.IUnknown)] object? pUnkOuter, uint dwClsContext, in Guid riid, [MarshalAs(UnmanagedType.IUnknown)] out object? ppv);
-    [DllImport("Ole32", SetLastError = true)]
-    static extern int CoInitialize(IntPtr pvReserved = default);
-    [DllImport("Dwmapi", SetLastError = true)]
-    static extern int DwmSetWindowAttribute(IntPtr hwnd, uint dwAttribute, void* pvAttribute, uint cbAttribute);
-
-
     // Fields.
-    ITaskbarList3? windowsTaskbarList;
+    Win32.ITaskbarList3? windowsTaskbarList;
 
 
     // Apply current theme mode on given window.
@@ -82,12 +27,12 @@ unsafe partial class AppSuiteApplication
             return;
         if (window.IsExtendedIntoWindowDecorations || window.SystemDecorations == Avalonia.Controls.SystemDecorations.None)
             return;
-        var hwnd = (window.TryGetPlatformHandle()?.Handle).GetValueOrDefault();
-        if (hwnd != default)
+        var hWnd = (window.TryGetPlatformHandle()?.Handle).GetValueOrDefault();
+        if (hWnd != default)
         {
             // setup window corner
-            var cornerPreference = DWMWCP_ROUND;
-            var result = DwmSetWindowAttribute(hwnd, DWMWA_WINDOW_CORNER_PREFERENCE, &cornerPreference, sizeof(int));
+            var cornerPreference = Win32.DWMWCP.ROUND;
+            var result = Win32.DwmSetWindowAttribute(hWnd, Win32.DWMWA.WINDOW_CORNER_PREFERENCE, &cornerPreference, sizeof(int));
             if (result != default)
             {
                 this.Logger.LogWarning("Failed to set corner preference of window '{title}', result: {result}", window.Title, result);
@@ -97,7 +42,7 @@ unsafe partial class AppSuiteApplication
 
             // enable/disable dark mode
             var darkMode = this.EffectiveThemeMode == ThemeMode.Dark;
-            result = DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &darkMode, sizeof(int) /* size of BOOL is same as DWORD */);
+            result = Win32.DwmSetWindowAttribute(hWnd, Win32.DWMWA.USE_IMMERSIVE_DARK_MODE, &darkMode, sizeof(int) /* size of BOOL is same as DWORD */);
             if (result != default)
             {
                 this.Logger.LogWarning("Failed to set dark mode of window '{title}', result: {result}", window.Title, result);
@@ -107,7 +52,7 @@ unsafe partial class AppSuiteApplication
             // setup title bar color
             var titleBarColor = this.FindResourceOrDefault<Color>("Color/Window.TitleBar");
             var win32Color = (titleBarColor.B << 16) | (titleBarColor.G << 8) | titleBarColor.R;
-            result = DwmSetWindowAttribute(hwnd, DWMWA_CAPTION_COLOR, &win32Color, sizeof(int));
+            result = Win32.DwmSetWindowAttribute(hWnd, Win32.DWMWA.CAPTION_COLOR, &win32Color, sizeof(int));
             if (result != default)
             {
                 this.Logger.LogWarning("Failed to set caption color of window '{title}', result: {result}", window.Title, result);
@@ -239,14 +184,14 @@ unsafe partial class AppSuiteApplication
     {
         if (this.windowsTaskbarList != null)
             return true;
-        CoInitialize();
-        var result = CoCreateInstance(in CLSID_TaskBarList, null, CLSCTX_INPROC_SERVER, in IID_TaskBarList3, out var obj);
+        Win32.CoInitialize();
+        var result = Win32.CoCreateInstance(in Win32.CLSID_TaskBarList, null, Win32.CLSCTX.INPROC_SERVER, in Win32.IID_TaskBarList3, out var obj);
         if (obj == null)
         {
             this.Logger.LogError("Unable to create ITaskBarList3 object, result: {result}", result);
             return false;
         }
-        this.windowsTaskbarList = obj as ITaskbarList3;
+        this.windowsTaskbarList = obj as Win32.ITaskbarList3;
         if (this.windowsTaskbarList == null)
         {
             this.Logger.LogError("Unable to get implementation of ITaskBarList3");
@@ -262,10 +207,10 @@ unsafe partial class AppSuiteApplication
     {
         if (!this.SetupWindowsTaskbarList())
             return;
-        var hwnd = (window.TryGetPlatformHandle()?.Handle).GetValueOrDefault();
-        if (hwnd == default)
+        var hWnd = (window.TryGetPlatformHandle()?.Handle).GetValueOrDefault();
+        if (hWnd == default)
             return;
-        this.windowsTaskbarList.SetProgressValue(hwnd, (ulong)(window.TaskbarIconProgress * 1000 + 0.5), 1000UL);
+        this.windowsTaskbarList.SetProgressValue(hWnd, (ulong)(window.TaskbarIconProgress * 1000 + 0.5), 1000UL);
     }
 
 
@@ -274,16 +219,16 @@ unsafe partial class AppSuiteApplication
     {
         if (!this.SetupWindowsTaskbarList())
             return;
-        var hwnd = (window.TryGetPlatformHandle()?.Handle).GetValueOrDefault();
-        if (hwnd == default)
+        var hWnd = (window.TryGetPlatformHandle()?.Handle).GetValueOrDefault();
+        if (hWnd == default)
             return;
-        this.windowsTaskbarList.SetProgressState(hwnd, window.TaskbarIconProgressState switch
+        this.windowsTaskbarList.SetProgressState(hWnd, window.TaskbarIconProgressState switch
         {
-            Controls.TaskbarIconProgressState.Error => TBPF_ERROR,
-            Controls.TaskbarIconProgressState.Indeterminate => TBPF_INDETERMINATE,
-            Controls.TaskbarIconProgressState.Normal => TBPF_NORMAL,
-            Controls.TaskbarIconProgressState.Paused => TBPF_PAUSED,
-            _ => TBPF_NOPROGRESS,
+            Controls.TaskbarIconProgressState.Error => Win32.TBPF.ERROR,
+            Controls.TaskbarIconProgressState.Indeterminate => Win32.TBPF.INDETERMINATE,
+            Controls.TaskbarIconProgressState.Normal => Win32.TBPF.NORMAL,
+            Controls.TaskbarIconProgressState.Paused => Win32.TBPF.PAUSED,
+            _ => Win32.TBPF.NOPROGRESS,
         });
     }
 }
--- Core/Native/Win32.cs
+++ Core/Native/Win32.cs
@@ -2,59 +2,135 @@
 using System.Runtime.InteropServices;
 using System.Text;
 
-namespace CarinaStudio.AppSuite.Native
+namespace CarinaStudio.AppSuite.Native;
+
+static unsafe class Win32
 {
-    internal static class Win32
+    public static readonly Guid CLSID_TaskBarList = new("56fdf344-fd6d-11d0-958a-006097c9a090");
+    public static readonly Guid IID_TaskBarList3 = new("ea1afb91-9e28-4b86-90e9-9e9f8a5eefaf");
+
+
+    public enum CLSCTX : uint
     {
-        public enum GWL : int
-        {
-            EXSTYLE = -20,
-            HINSTANCE = -6,
-            HWNDPARENT = -8,
-            ID = -12,
-            STYLE = -16,
-            USERDATA = -21,
-            WNDPROC = -4,
-        }
-        
-        
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
-        {
-            public int left;
-            public int top;
-            public int right;
-            public int bottom;
-        }
-
-
-        [Flags]
-        public enum WS : int
-        {
-            CAPTION	= 0x00C00000,
-            MAXIMIZEBOX = 0x00010000,
-            MINIMIZEBOX	= 0x00020000,
-            SYSMENU	= 0x00080000,
-        }
-
-
-        [DllImport("Kernel32")]
-        public static extern uint GetModuleFileName(IntPtr hModule, StringBuilder lpFilename, uint nSize);
-
-
-        [DllImport("User32")]
-        public static extern nint GetWindowLong(IntPtr hWnd, GWL nIndex);
-
-
-        [DllImport("User32")]
-        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
-
-
-        [DllImport("User32")]
-        public static extern bool SetForegroundWindow(IntPtr hWnd);
-
-
-        [DllImport("User32")]
-        public static extern nint SetWindowLong(IntPtr hWnd, GWL nIndex, nint dwNewLong);
+        INPROC_SERVER = 0x1,
     }
+    
+    
+    public enum DWMWA : uint
+    {
+        CAPTION_COLOR = 35,
+        WINDOW_CORNER_PREFERENCE = 33,
+        USE_IMMERSIVE_DARK_MODE = 20,
+    }
+
+    public enum DWMWCP
+    {
+        ROUND = 2,
+    }
+
+    public enum GWL : int
+    {
+        EXSTYLE = -20,
+        HINSTANCE = -6,
+        HWNDPARENT = -8,
+        ID = -12,
+        STYLE = -16,
+        USERDATA = -21,
+        WNDPROC = -4,
+    }
+    
+    
+    [ComImport]
+    [Guid("ea1afb91-9e28-4b86-90e9-9e9f8a5eefaf")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface ITaskbarList3
+    {
+        // ITaskbarList
+        void HrInit();
+        void AddTab(IntPtr hwnd);
+        void DeleteTab(IntPtr hwnd);
+        void ActivateTab(IntPtr hwnd);
+        void SetActiveAlt(IntPtr hwnd);
+
+        // ITaskbarList2
+        void MarkFullscreenWindow(IntPtr hwnd, bool fFullscreen);
+
+        // ITaskbarList3
+        void SetProgressValue(IntPtr hwnd, ulong ullCompleted, ulong ullTotal);
+        void SetProgressState(IntPtr hwnd, Win32.TBPF tbpFlags);
+        void RegisterTab(IntPtr hwndTab, IntPtr hwndMDI);
+        void UnregisterTab(IntPtr hwndTab);
+        void SetTabOrder(IntPtr hwndTab, IntPtr hwndInsertBefore);
+        void SetTabActive(IntPtr hwndTab, IntPtr hwndMDI);
+        void ThumbBarAddButtons(IntPtr hwnd, uint cButtons, IntPtr pButton);
+        void ThumbBarUpdateButtons(IntPtr hwnd, uint cButtons, IntPtr pButton);
+        void ThumbBarSetImageList(IntPtr hwnd, IntPtr himl);
+        void SetOverlayIcon(IntPtr hwnd, IntPtr hIcon, string? pszDescription);
+        void SetThumbnailTooltip(IntPtr hwnd, string? pszTip);
+        void SetThumbnailClip(IntPtr hwnd, void* prcClip);
+    }
+    
+    
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+    }
+
+
+    [Flags]
+    public enum TBPF : uint
+    {
+        ERROR = 0x4,
+        INDETERMINATE = 0x1,
+        NOPROGRESS = 0x0,
+        NORMAL = 0x2,
+        PAUSED = 0x8,
+    }
+
+
+    [Flags]
+    public enum WS : int
+    {
+        CAPTION	= 0x00C00000,
+        MAXIMIZEBOX = 0x00010000,
+        MINIMIZEBOX	= 0x00020000,
+        SYSMENU	= 0x00080000,
+    }
+    
+    
+    [DllImport("Ole32", SetLastError = true)]
+    public static extern int CoCreateInstance(in Guid rclsid, [MarshalAs(UnmanagedType.IUnknown)] object? pUnkOuter, CLSCTX dwClsContext, in Guid riid, [MarshalAs(UnmanagedType.IUnknown)] out object? ppv);
+    
+    
+    [DllImport("Ole32", SetLastError = true)]
+    public static extern int CoInitialize(IntPtr pvReserved = default);
+    
+    
+    [DllImport("Dwmapi", SetLastError = true)]
+    public static extern int DwmSetWindowAttribute(IntPtr hwnd, DWMWA dwAttribute, void* pvAttribute, uint cbAttribute);
+
+
+    [DllImport("Kernel32", SetLastError = true)]
+    public static extern uint GetModuleFileName(IntPtr hModule, StringBuilder lpFilename, uint nSize);
+
+
+    [DllImport("User32", SetLastError = true)]
+    public static extern nint GetWindowLong(IntPtr hWnd, GWL nIndex);
+
+
+    [DllImport("User32", SetLastError = true)]
+    public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+
+    [DllImport("User32", SetLastError = true)]
+    public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+
+    [DllImport("User32", SetLastError = true)]
+    public static extern nint SetWindowLong(IntPtr hWnd, GWL nIndex, nint dwNewLong);
 }
+
